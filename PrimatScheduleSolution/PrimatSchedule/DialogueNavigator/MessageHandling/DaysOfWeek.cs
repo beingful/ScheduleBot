80e71fdef24ad918dd99295c34cf4870710c3fe5
Hanna1-droid <anna.bananna.kasayka@gmail.com>
2022-05-14 00:03:37 +0300

    Clean program from comments and trash files

--- PrimatScheduleSolution/PrimatSchedule/DialogueNavigator/MessageHandling/DaysOfWeek.cs
+++ PrimatScheduleSolution/PrimatSchedule/DialogueNavigator/MessageHandling/DaysOfWeek.cs
@@ -13,33 +13,8 @@ namespace PrimatScheduleBot
             _days = CultureInfo.GetCultureInfo("uk-UA").DateTimeFormat.DayNames;
             FormatFriday(ref _days[5]);
         }
-
-        //private static bool AreEqual(string day, string anotherDay)
-        //{
-        //    int index = Array.IndexOf(_days, day);
-
-        //    //Console.WriteLine(FormateDay(day, index));
-        //    //Console.WriteLine(FormateDay(anotherDay, index));
-
-        //    return FormateDay(day, index) == FormateDay(anotherDay, index);
-        //}
-
+        
         private static void FormatFriday(ref string day) => day = day.Remove(1, 1).Insert(1, $"{(char)39}");
-
-        //private static string FormateDay(string day, int index)
-        //{
-        //    day = day.ToLower();
-
-        //    if (index is 5)
-        //    {
-        //        day = RemoveSymbols(day);
-        //    }
-
-        //    return day;
-        //}
-
-        //private static string RemoveSymbols(string day) 
-        //    => String.Join(String.Empty, day.Where(letter => (int)letter != 700 && (int)letter != 39));
 
         public static bool DoesSuchADayExist(string day) 
             => _days.Any(dayOfWeek => dayOfWeek == day.ToLower());
